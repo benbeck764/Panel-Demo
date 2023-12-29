7ea22d2fa4d88321f27c72874ae108947ecd99f3
Ben Beck <ben.beck@trueanomaly.space>
2023-12-29 10:44:08 -0600

    Cleaning up React App Unity display

--- panel-app/src/features/unity/Unity.styles.ts
+++ panel-app/src/features/unity/Unity.styles.ts
@@ -1,6 +1,12 @@
+import Box from "@mui/material/Box";
 import { styled } from "@mui/material/styles";
 
-export const StyledIframe = styled("iframe")(() => ({
+export const StyledIFrameWrapper = styled(Box)(({theme}) => ({
+  width: '100%',
+  height: `calc(100vh - ${theme.headerHeights?.xl}px - 8px)`
+}));
+
+export const StyledIFrame = styled("iframe")(() => ({
   border: "none",
   overflow: "hidden",
 }));
--- panel-app/src/features/unity/Unity.tsx
+++ panel-app/src/features/unity/Unity.tsx
@@ -1,35 +1,55 @@
+import AppButton from "@benbeck764/react-components/Button";
 import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
 import { FC, createRef } from "react";
-import { StyledIframe } from "./Unity.styles";
+import { StyledIFrame, StyledIFrameWrapper } from "./Unity.styles";
 
 const Unity: FC = () => {
   const iframeRef = createRef<HTMLIFrameElement>();
 
-  // const toggleFullscreen = (): void => {
-  //   const iframe = iframeRef.current;
-  //   if (!iframe) return;
+  const toggleFullscreen = (): void => {
+    const iframe = iframeRef.current;
+    if (!iframe) return;
 
-  //   if (iframe.requestFullscreen) {
-  //     iframe.requestFullscreen();
-  //   }
-  // };
+    if (iframe.requestFullscreen) {
+      iframe.requestFullscreen();
+    }
+  };
 
   return (
-    <Box>
-      {/* <Stack direction="row" justifyContent="flex-end">
-        <AppButton onClick={toggleFullscreen}>Toggle Fullscreen</AppButton>
-      </Stack> */}
-      <Box width="100%" height="100%">
-        <StyledIframe
+    <Box position="relative">
+      <StyledIFrameWrapper>
+        <StyledIFrame
           ref={iframeRef}
           title="Unity WebGL"
           src="/WebGL_Build/index.html"
           width="100%"
-          height="1000"
+          height="100%"
           scrolling="no"
           seamless={true}
           allowFullScreen={true}
         />
+      </StyledIFrameWrapper>
+      <Box
+        sx={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          zIndex: 2,
+          width: "100%",
+        }}
+        p={2}
+      >
+        <Stack
+          direction="row"
+          width="100%"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <Typography variant="h4">Testing Overlay</Typography>
+          <AppButton onClick={toggleFullscreen}>Toggle Fullscreen</AppButton>
+        </Stack>
       </Box>
     </Box>
   );
